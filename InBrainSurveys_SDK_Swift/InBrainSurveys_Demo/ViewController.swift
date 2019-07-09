604b2cf2317f91e86862b1562f69f23597bfd33b
Joel Myers <joel@appsthatpay.co>
2019-07-09 16:02:16 -0400

    SDK updated with UI functions for WebView; Packaged version works on Simulator and Device

--- InBrainSurveys_SDK_Swift/InBrainSurveys_Demo/ViewController.swift
+++ InBrainSurveys_SDK_Swift/InBrainSurveys_Demo/ViewController.swift
@@ -42,10 +42,10 @@ class ViewController: UIViewController, InBrainDelegate {
     }
 
     @IBAction func showInBrain(_ sender: Any) {
-//        inBrain.setInBrainWebViewNavBarColor(toColor: UIColor.green)
-//        inBrain.setInBrainWebViewTitle(toString: "Little Surveys")
-//        inBrain.setInBrainWebViewNavButtonColor(toColor: UIColor.yellow)
-        inBrain.presentInBrainWebView(withAppUID: "hahaDavis")
+        inBrain.setInBrainWebViewNavBarColor(toColor: UIColor.green)
+        inBrain.setInBrainWebViewTitle(toString: "Little Surveys")
+        inBrain.setInBrainWebViewNavButtonColor(toColor: UIColor.yellow)
+        inBrain.presentInBrainWebView(withAppUID: "GlenDavis")
        
         //        InBrain.presentInBrainWebView(withClientID: "external-web-client", clientSecret: ViewController.stagingKey, andAppUID: "jie@atp.co")
 //        InBrain.presentInBrainWebView(withAppUID: "tes@test.com")
