8baa6f398a05fbd313d7588f1930e220bd209a24
Sergey Blazhko <s.blazhko.dev@gmail.com>
2022-07-08 10:20:35 +0300

    Ver 1.8.7: - SurveyProfileMatch values updated

--- InBrainSurveys_SDK_Swift.xcframework/ios-arm64_x86_64-maccatalyst/InBrainSurveys_SDK_Swift.framework/Versions/A/Headers/InBrainSurveys_SDK_Swift-Swift.h
+++ InBrainSurveys_SDK_Swift.xcframework/ios-arm64_x86_64-maccatalyst/InBrainSurveys_SDK_Swift.framework/Versions/A/Headers/InBrainSurveys_SDK_Swift-Swift.h
@@ -512,6 +512,7 @@ SWIFT_PROTOCOL("_TtP24InBrainSurveys_SDK_Swift20NativeSurveyDelegate_")
 - (void)failedToReceiveNativeSurveysWithError:(NSError * _Nonnull)error placementId:(NSString * _Nullable)placementId;
 @end
 
+/// Indicates how the survey match user’s profile
 typedef SWIFT_ENUM(NSInteger, SurveyProfileMatch, open) {
   SurveyProfileMatchNewSurvey = 0,
   SurveyProfileMatchPoorMatch = 1,
@@ -1042,6 +1043,7 @@ SWIFT_PROTOCOL("_TtP24InBrainSurveys_SDK_Swift20NativeSurveyDelegate_")
 - (void)failedToReceiveNativeSurveysWithError:(NSError * _Nonnull)error placementId:(NSString * _Nullable)placementId;
 @end
 
+/// Indicates how the survey match user’s profile
 typedef SWIFT_ENUM(NSInteger, SurveyProfileMatch, open) {
   SurveyProfileMatchNewSurvey = 0,
   SurveyProfileMatchPoorMatch = 1,
